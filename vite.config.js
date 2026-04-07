e12219118bd7ff11adf2c576c142c41766f31b6e
Sebastien Chemin <seb@chemin.io>
2026-04-07 12:45:38 +0200

    Revert base path to ./ for Netlify deployment

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/music-theory-game/',
+  base: './',
   resolve: {
     alias: {
       '@': '/src',
