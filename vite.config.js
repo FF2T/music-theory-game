12d380e72d5f52fe1f50ebda0338b0addfb71e5c
Sebastien Chemin <seb@chemin.io>
2026-04-07 13:00:15 +0200

    Set base path for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: '/music-theory-game/',
   resolve: {
     alias: {
       '@': '/src',
